da1febbfa206d58d1341ec5b2d5e75daf14659c1
Clayton Rafael <clayton.rssouza@gmail.com>
2022-03-19 14:30:07 -0300

    feat: implement global style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import styles from '../styles/home.module.scss'
 
 export default function Home() {
   return (
@@ -7,7 +6,7 @@ export default function Home() {
       <Head>
         <title>IgNews - Home</title>
       </Head>
-      <h1 className={styles.title}>Hello World</h1>
+      <h1>Hello World</h1>
     </>
   )
 }
