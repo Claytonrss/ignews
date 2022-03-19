dd049bd6f16594fd9dd1da91189cabf63ab5576d
Clayton Rafael <clayton.rssouza@gmail.com>
2022-03-19 14:18:27 -0300

    feat: implement title per page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,13 @@
+import Head from 'next/head'
 import styles from '../styles/home.module.scss'
 
 export default function Home() {
-  return <h1 className={styles.title}>Hello World</h1>
+  return (
+    <>
+      <Head>
+        <title>IgNews - Home</title>
+      </Head>
+      <h1 className={styles.title}>Hello World</h1>
+    </>
+  )
 }
