414745f2acb4fa5c3c54f867c94576334bdf0abf
Clayton Rafael <clayton.rssouza@gmail.com>
2022-03-20 14:37:36 -0300

    feat: implement checkout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,7 +36,9 @@ export default function Home({ product }: HomeProps) {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const price = await stripe.prices.retrieve('price_1Kf89zDWWXWJKOpfBEoC3zd8')
+  const price = await stripe.prices.retrieve(
+    process.env.STRIPE_SUBSCRIPTION_PRICE_ID
+  )
 
   const product = {
     priceId: price.id,
