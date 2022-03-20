13ef2f6543aff72b89e6541a9525e42a63d2364b
Clayton Rafael <clayton.rssouza@gmail.com>
2022-03-20 11:10:53 -0300

    feat: implement auth and integrate faunadb

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,38 @@
+import { query as q } from 'faunadb'
+import NextAuth from 'next-auth'
+import GithubProvider from 'next-auth/providers/github'
+import { fauna } from '../../../services/fauna'
+
+export default NextAuth({
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      authorization: {
+        params: {
+          scope: 'read:user user:email',
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    async signIn({ user }) {
+      try {
+        const { email } = user
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(q.Match(q.Index('user_by_email'), q.Casefold(email)))
+            ),
+            q.Create(q.Collection('users'), { data: { email } }),
+            q.Get(q.Match(q.Index('user_by_email'), q.Casefold(email)))
+          )
+        )
+
+        return true
+      } catch (error) {
+        return false
+      }
+    },
+  },
+})
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,17 @@
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import { SubscribeButton } from '../components/SubscribeButton'
+import { stripe } from '../services/stripe'
 import styles from './home.module.scss'
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    productId: string
+    amount: number
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -16,12 +25,31 @@ export default function Home() {
           </h1>
           <p>
             Get all to all publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.productId} />
         </section>
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1Kf89zDWWXWJKOpfBEoC3zd8')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  }
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24 * 30, //30 days
+  }
 }
